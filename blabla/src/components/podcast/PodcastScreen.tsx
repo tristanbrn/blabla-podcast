8c79669fa29283d1d2bd3ed750a84ef2f855b22e
Tristan <tristan@MacBook-Pro-de-Tristan.local>
2020-05-13 18:00:05 +0200

    podcast feed from itune Api

--- blabla/src/components/podcast/PodcastScreen.tsx
+++ blabla/src/components/podcast/PodcastScreen.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Box, Text } from 'react-native-design-utility';
 import { useRoute, RouteProp } from '@react-navigation/native';
 import { IPodcast } from '../../types/Podcast';
-import { Image } from 'react-native';
-import { feedUrlServices } from '../services/FeedUrlServices';
+import { Image, ActivityIndicator } from 'react-native';
+import { feedUrlServices } from '../../services/FeedUrlServices';
+import { Feed } from 'react-native-rss-parser'
+import { theme } from '../../constants/theme';
+import { ScrollView } from 'react-native-gesture-handler';
 
 type PodcastScreenRouteProp = RouteProp<
     { Podcast: { podcast: IPodcast } },
@@ -12,26 +15,54 @@ type PodcastScreenRouteProp = RouteProp<
 
 const PodcastScreen: React.FC = () => {
     const { params } = useRoute<PodcastScreenRouteProp>();
+    const [feed, setFeed] = React.useState<Feed | null>(null)
 
     React.useEffect( () => {
-        feedUrlServices.getFeed(params.podcast.feedUrl)
-            .then(result => {
+        feedUrlServices.getFeed(params.podcast.feedUrl).then(result => {
+            setFeed(result)
+        })
+    }, [])
 
-            })
-    } [])
+    if(!feed) {
+        return (
+            <Box f={1} bg="white" center>
+                <ActivityIndicator color={theme.color.blueDarker} size="large" />
+            </Box>
+        )
+    }
 
     return (
         <Box f={1} bg="white">
-            <Box dir="row" pr="sm">
-                <Box h={100} w={100} mr="xs">
-                    <Image 
-                        source={{ uri: params.podcast.artworkUrl100 }} 
-                        style={{ flex:1 }}
-                    />
-                </Box>
-            </Box>
-            <Box f={1}>
-                <Text numberOfLines={1}>{params.podcast.trackName}</Text>
+            <Box>
+                <ScrollView>
+                    <Box dir="row" pr="sm" mb="sm">
+                        <Box h={100} w={100} mr="xs">
+                            <Image 
+                                source={{ uri: params.podcast.artworkUrl100 }} 
+                                style={{ flex:1 }}
+                            />
+                        </Box>
+                        <Box f={1}>
+                            <Text numberOfLines={1}>{params.podcast.trackName}</Text>
+                        </Box>
+                    </Box>
+                    <Box px="sm">
+                        <Text>{feed?.description}</Text>
+                    </Box>
+                    {feed?.items.map(item => (
+                        <Box key={item.id}>
+                            <Box dir="row">
+                                <Box h={75} w={75} mr="xs">
+                                    <Image source={{ uri: item.itunes.image }} style={{ flex: 1 }} />
+                                </Box>
+                                <Box>
+                                    <Text>{item.title}</Text>
+                                </Box>
+                            </Box>
+                            <Box h={1} w="100%" bg="red" />
+                        </Box>
+                    ))}
+                </ScrollView>
             </Box>
         </Box>
     );
